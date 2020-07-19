f2587b3628d2bc3721f3856d417fdf1b1a468951
Jason <Jasonfrancis1997@hotmail.com>
2020-07-19 18:53:05 +0100

    push

--- lib/binding/manager.go
+++ lib/binding/manager.go
@@ -115,15 +115,15 @@ func (b *Manager) generateTypescriptDefinitions() error {
 
 	for structname, methodList := range b.structList {
 		structname = strings.SplitN(structname, ".", 2)[1]
-		output.WriteString(fmt.Sprintf("Interface %s {\n", structname))
+		output.WriteString(fmt.Sprintf("interface %s {\n", structname))
 		for _, method := range methodList {
-			output.WriteString(fmt.Sprintf("\t%s: (...args : any[]) => Promise\n", method))
+			output.WriteString(fmt.Sprintf("\t%s: (...args : any[]) => Promise<any>\n", method))
 		}
 		output.WriteString("}\n")
 	}
 
 	output.WriteString("\n")
-	output.WriteString("Interface Backend {\n")
+	output.WriteString("interface Backend {\n")
 
 	for structname := range b.structList {
 		structname = strings.SplitN(structname, ".", 2)[1]
@@ -132,10 +132,8 @@ func (b *Manager) generateTypescriptDefinitions() error {
 	output.WriteString("}\n")
 
 	globals := `
-declare global {
-	interface Window {
-		backend: Backend;
-	}
+interface Window {
+	backend: Backend;
 }`
 	output.WriteString(globals)
 
